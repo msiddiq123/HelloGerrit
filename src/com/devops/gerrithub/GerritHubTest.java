8f0097bd6b302feda03faff0d51b33045e93bc4e
msiddiq123 <maroof.siddique2013@gmail.com>
2017-09-10 00:12:33 +0530

    First gerrit commit

---
+++ src/com/devops/gerrithub/GerritHubTest.java
@@ -0,0 +1,11 @@
+package com.devops.gerrithub;
+
+public class GerritHubTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("------Gerrit Hub Test-1-------on------9-SEP-2017");
+	}
+
+}
