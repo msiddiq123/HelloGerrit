fb245667ecb84a83d741fa8985f27fd9c99248e4
msiddiq123 <maroof.siddique2013@gmail.com>
2017-09-15 15:01:54 +0530

    change-1 on sep-15

--- src/com/devops/gerrithub/GerritHubTest.java
+++ src/com/devops/gerrithub/GerritHubTest.java
@@ -5,7 +5,7 @@ public class GerritHubTest {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		System.out.println("------Gerrit Hub Testing-4-------on------14-SEP-2017");
+		System.out.println("------Gerrit Hub Testing-5-------on------15-SEP-2017");
 	}
 
 }
